608dcc6c5e5109125bd49b96aa7a02a5d2c1c950
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-04-25 19:29:32 -0500

    Updated manifest.json to contain all icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,8 +2,15 @@ var CACHE_NAME = "Budget_v1";
 var DATA_CACHE_NAME = "Budget_v1";
 
 var FILES_TO_CACHE = [
-    '/index.html', '/manifest.json', '/js/index.js', '/js/idb.js', '/css/styles.css',
-    '/icons/icon-192x192.png', '/icons/icon-96x96.png' 
+    '/index.html',
+    '/manifest.json', 
+    '/js/index.js', 
+    '/js/idb.js', 
+    '/css/styles.css',
+    '/icons/icon-192x192.png', 
+    '/icons/icon-96x96.png',
+    '/icons/icon-72x72.png',
+     
 ];
 
 // install SELF add eventListener install 
