ec0a3d1f60c8bfd5ee0a6261a38968a0c8694692
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-04-25 21:51:12 -0500

    added rest of files to cache array, fetch and install event listeners to service-worker.js file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
-var CACHE_NAME = "Budget_v1";
-var DATA_CACHE_NAME = "Budget_v1";
+const CACHE_NAME = "Budget_v1";
+const DATA_CACHE_NAME = "Budget_v1";
 
-var FILES_TO_CACHE = [
+const FILES_TO_CACHE = [
     '/index.html',
     '/manifest.json', 
     '/js/index.js', 
@@ -10,8 +10,39 @@ var FILES_TO_CACHE = [
     '/icons/icon-192x192.png', 
     '/icons/icon-96x96.png',
     '/icons/icon-72x72.png',
-     
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
 ];
 
-// install SELF add eventListener install 
-// install FETCH add eventListener install
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  })
+
+// Cache resources
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  })
+  
